6ce82317b760a2b3ff4a79c841e6832dd1131b28
unknown <you@example.com>
2018-07-14 23:56:58 +0200

    antd integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.js';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+import './style.css';
+
+const { Header, Footer, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
+      <Layout>
       <div>
-        <header>
+        <Header>
           <h1>Welcome to React</h1>
-        </header>
-        <p >
+        </Header>
+        <Content>
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </Content>
       </div>
+      <Footer>
+        </Footer>
+      </Layout>
     );
   }
 }
